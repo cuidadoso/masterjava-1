8ad107466a38eab42e37a90973b6c2f00af03fe6
cuidadoso <alexander_s@front.ru>
2017-03-30 16:23:39 +0200

    5 5 typesafe config

--- persist/src/test/java/ru/javaops/masterjava/persist/DBITestProvider.java
+++ persist/src/test/java/ru/javaops/masterjava/persist/DBITestProvider.java
@@ -1,5 +1,8 @@
 package ru.javaops.masterjava.persist;
 
+import com.typesafe.config.Config;
+import ru.javaops.masterjava.config.common.Configs;
+
 import java.sql.DriverManager;
 
 /**
@@ -8,7 +11,9 @@ import java.sql.DriverManager;
  */
 public class DBITestProvider {
     public static void initDBI() {
-        initDBI("jdbc:postgresql://localhost:5434/masterjava", "postgres", "postgres");
+        //initDBI("jdbc:postgresql://localhost:5434/masterjava", "postgres", "postgres");
+        Config db = Configs.getConfig("persist.conf","db");
+        initDBI(db.getString("url"), db.getString("user"), db.getString("password"));
     }
 
     public static void initDBI(String dbUrl, String dbUser, String dbPassword) {
