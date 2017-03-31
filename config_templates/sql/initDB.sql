5ed9609e774a6adc26f87cb55d8ccf6ca9acd956
cuidadoso <alexander_s@front.ru>
2017-03-31 16:37:45 +0200

    HW05 Add SQL scripts to initiate tables.

--- config_templates/sql/initDB.sql
+++ config_templates/sql/initDB.sql
@@ -1,3 +1,4 @@
+/* Users */
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS user_seq;
 DROP TYPE IF EXISTS user_flag;
@@ -14,3 +15,44 @@ CREATE TABLE users (
 );
 
 CREATE UNIQUE INDEX email_idx ON users (email);
+
+/* Projects */
+DROP TABLE IF EXISTS projects;
+DROP SEQUENCE IF EXISTS project_seq;
+
+CREATE SEQUENCE project_seq START 200000;
+
+CREATE TABLE projects (
+  id        INTEGER PRIMARY KEY DEFAULT nextval('project_seq'),
+  name      TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX project_idx ON projects (name);
+
+/* City */
+DROP TABLE IF EXISTS cities;
+DROP SEQUENCE IF EXISTS city_seq;
+
+CREATE SEQUENCE city_seq START 300000;
+
+CREATE TABLE cities (
+  id        INTEGER PRIMARY KEY DEFAULT nextval('city_seq'),
+  id_name   TEXT NOT NULL,
+  name      TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX id_name_idx ON cities (id_name);
+
+/* Groups */
+DROP TABLE IF EXISTS groups;
+DROP SEQUENCE IF EXISTS group_seq;
+
+CREATE SEQUENCE group_seq START 400000;
+
+CREATE TABLE groups (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('group_seq'),
+  name        TEXT NOT NULL,
+  project_id  INTEGER
+);
+
+CREATE UNIQUE INDEX group_idx ON groups (name);
