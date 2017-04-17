4bb745645b3f4b65475719a3dede8ec977b31514
cuidadoso <alexander_s@front.ru>
2017-04-17 17:11:16 +0200

    7 10 send group bulk

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -1,6 +1,5 @@
 package ru.javaops.masterjava.service.mail;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
 import javax.xml.namespace.QName;
@@ -18,8 +17,15 @@ public class MailServiceClient {
         MailService mailService = service.getPort(MailService.class);
         //mailService.sendMail(ImmutableList.of(new Addressee("alexander_s@front.ru")), ImmutableList.of(), "Subject", "Body");
 
-        mailService.sendMail(
+
+        mailService.sendToGroup(ImmutableSet.of(
+                new Addressee("gkislin@javaops.ru"),
+                new Addressee("Bad Email <bad_email.ru>")), ImmutableSet.of(), "Subject", "Body");
+
+
+        mailService.sendToGroup(
                 ImmutableSet.of(new Addressee("Григорий Кислин <gkislin@javaops.ru>")),
                 ImmutableSet.of(new Addressee("Мастер Java <masterjava@javaops.ru>")), "Subject", "Body");
+
     }
 }
